dab1f1db621077e3db3d6d9f287ffb7b631735f8
S Mohammad Reza <info@shahrestani.co.uk>
2022-04-22 07:20:05 +0100

    update requrements.txt

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-58u-6l91*$7x1rgfdb5#h)^bbxyqmy))9@$gip%4fur1(2e)@p
 
 
 # Enable debug environment
-DEBUG = True
+DEBUG = False
 
 # List of allowed hosts
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
